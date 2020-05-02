3028ce421ea060608b5046581299cd3034403ca0
Anton <antonderegt@pm.me>
2020-05-02 16:40:49 +0200

    Inital commit

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,109 @@
+<template>
+  <table v-if="showCategory">
+    <tr>
+      <th v-if="!editTitle" @click="editTitle = true">{{ category }}</th>
+      <th v-else>
+        <input v-model="category" @keyup.enter="changeTitle" type="text" />
+        <button @click="changeTitle" class="add">V</button>
+      </th>
+      <button @click="deleteCategory" class="delete">X</button>
+    </tr>
+    <tr v-for="(item, index) in items" :key="item">
+      <td
+        v-if="editItem != index"
+        @click="
+          editItem = index;
+          changedItem = item;
+        "
+      >
+        {{ item }}<button @click="deleteItem(index)" class="delete">X</button>
+      </td>
+      <td v-else>
+        <input v-model="changedItem" @keyup.enter="changeItem" type="text" />
+        <button @click="changeItem" class="add">V</button>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input
+          v-model="newItem"
+          @keyup.enter="addItem()"
+          type="text"
+          placeholder="new item..."
+        />
+        <button @click="addItem" class="add">V</button>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    category: String,
+    items: Array
+  },
+  data: function() {
+    return {
+      showCategory: true,
+      newItem: "",
+      editTitle: false,
+      editItem: -1,
+      changedItem: ""
+    };
+  },
+  methods: {
+    changeTitle: function() {
+      this.editTitle = false;
+    },
+    changeItem: function() {
+      this.items[this.editItem] = this.changedItem;
+      this.editItem = -1;
+    },
+    addItem: function() {
+      if (this.newItem == "") alert("You have to enter something first!");
+      else {
+        this.items.push(this.newItem);
+        this.newItem = "";
+      }
+    },
+    deleteItem: function(index) {
+      this.items.splice(index, 1);
+    },
+    deleteCategory: function() {
+      this.category = "";
+      this.items = [];
+      this.showCategory = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  /* width: calc(100% - 40px); */
+  border: 0;
+  padding: 6px;
+  font-size: 1.1em;
+  background-color: #6dbfbf;
+  color: rgb(38, 34, 34);
+}
+
+.delete {
+  background-color: tomato;
+  border: 0;
+  border-radius: 15%;
+  color: whitesmoke;
+  font-weight: bold;
+  margin: 5px;
+}
+
+.add {
+  background-color: yellowgreen;
+  border: 0;
+  border-radius: 15%;
+  color: whitesmoke;
+  font-weight: bold;
+  margin: 5px;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Problem parts table</h1>
+    <CategoryCard
+      v-for="cat in categories"
+      :category="cat.name"
+      :items="cat.items"
+      :key="cat.name"
+    />
+    <button @click="addCategory">Add Category</button>
   </div>
 </template>
+
+<script>
+import CategoryCard from "@/components/CategoryCard.vue";
+
+export default {
+  data: function() {
+    return {
+      categories: [
+        {
+          name: "cat 1",
+          items: ["test 1", "test 2"]
+        },
+        {
+          name: "cat 2",
+          items: ["test 1", "test 2", "test 3"]
+        }
+      ]
+    };
+  },
+  components: {
+    CategoryCard
+  },
+  methods: {
+    addCategory: function() {
+      this.categories.push({ name: "New Cat", items: ["item 1"] });
+    }
+  }
+};
+</script>
